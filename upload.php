8db43303cf7a2b5ef09cf302017bde245d2fc4c9
William <116270884+hengkerr@users.noreply.github.com>
2023-08-09 14:30:18 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -12,9 +12,29 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Hanya gambar JPEG dan PNG!";
         exit;
     } if (move_uploaded_file($_FILES["image"]["tmp_name"], $targetFile)) {
-        echo "File uploaded successfully.";
+        $dbHost = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbName = "fileupload";
+
+        $conn = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $stmt = $conn->prepare("INSERT INTO uploads (email, image_path) VALUES (?, ?)");
+        $stmt->bind_param("ss", $email, $targetFile);
+
+        if ($stmt->execute()) {
+            echo "File berhasil diunggah.";
+        } else {
+            echo "Error storing data!";
+        }
+
+        $stmt->close();
+        $conn->close();
     } else {
-        echo "Error uploading file!";
+        echo "Gagal Mengunggah File!";
     }
 }
 ?>
